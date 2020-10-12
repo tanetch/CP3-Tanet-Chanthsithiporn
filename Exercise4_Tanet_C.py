e2ce16a9b7f95feb687bbd2be1e40bda14142181
Profextion <72730840+tanetch@users.noreply.github.com>
2020-10-12 16:10:50 +0700

    Add files via upload

---
+++ Exercise4_Tanet_C.py
@@ -0,0 +1,9 @@
+fe =80
+gb =85
+cs =81
+cp =84
+print("--- Your Score ---")
+print("Foundation English               : ",fe)
+print("General Business                 : ",gb)
+print("Introduction to Computer System  : ",cs)
+print("Computer Programming             : ",fe)
